869000cc9bd8249a0afbb2b9a4259573c5ba48ea
samgongora26 <saul.santiago2426@gmail.com>
2020-07-29 17:06:04 -0500

    formulario de agregar ciclos, configuracion y ver

--- admin/ciclos.php
+++ admin/ciclos.php
@@ -25,6 +25,7 @@
   <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
   <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
+  
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -68,8 +69,8 @@
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Bienvenido  _</h1>
+          <div class="col-sm-6  ">
+            <h1 class="m-0 text-dark mx-auto">Ciclos</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
           </div><!-- /.col -->
@@ -82,7 +83,130 @@
     <section class="content">
       <div class="container-fluid">
         <!-- Main row -->
-        
+        <div class="row">
+            <div class="col-md-12 col-lg-10 mx-auto col-s-12">
+                <!--------------ESTE CICLO------->
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Configuración del ciclo actual</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5>Ciclo actual: junio - agosto </h5>
+                        <h6>Periodo </h6>
+                        <!--   SELECTOR DE FECHA
+                        <div class="form-group">
+                          <label>Date:</label>
+                            <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                                <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
+                                <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        -->
+                        <!-- /.form group -->
+                        <!-- Date range -->
+                        <div class="form-group">
+                          <label>Rango de fechas:</label>
+                          <div class="input-group">
+                              <div class="input-group-prepend">
+                              <span class="input-group-text">
+                                  <i class="far fa-calendar-alt"></i>
+                              </span>
+                              </div>
+                              <input type="text" class="form-control float-right" id="reservation">
+                          </div>
+                          <!-- /.input group -->
+                        </div>
+                        <p style="color:red;"> Cambiar a ciclo inactivo </p>
+                        <p> Esta accion cambia toda la arquitectura de los alumnos, docentes, licenciaturas, calificaciones y demás </p>
+                        <input type="checkbox" name="my-checkbox" checked data-bootstrap-switch data-off-color="danger" data-on-color="success">
+                        <button type="submit" class="btn btn-danger float-right">Guardar Cambios</button>
+                    </div>
+                </div>
+                <!--Fin Este ciclo----->
+                <!-----------------------------------------AGREGAR CICLO NUEVO ------------------------------>
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Agregar un cliclo nuevo</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5>Ciclo actual: junio - agosto </h5>
+                        <h6>Periodo </h6>
+                        <!-- Date range -->
+                        <div class="form-group">
+                          <label>Rango de fechas:</label>
+                          <div class="input-group">
+                              <div class="input-group-prepend">
+                              <span class="input-group-text">
+                                  <i class="far fa-calendar-alt"></i>
+                              </span>
+                              </div>
+                              <input type="text" class="form-control float-right" id="reservation">
+                          </div>
+                          <!-- /.input group -->
+                        </div>
+                        <p style="color:red;">1. Cambiar a ciclo activo </p>
+                        <p> 1.1Esta accion cambia toda la arquitectura de los alumnos, docentes, licenciaturas, calificaciones y demás campos. aplicar solo cuando haya finalizado el ciclo anterior </p>
+                        <p style="color:red;">2. Sólo cambia a activo si no hay otro ciclo activo </p>
+                        <p style="color:red;">3. Si hay un ciclo activo se guardará como ciclo futuro </p>
+                        <input type="checkbox"  name="my-checkbox" checked data-bootstrap-switch data-off-color="danger" data-on-color="success">
+                        <button type="submit" class="btn btn-danger float-right">Guardar Cambios</button>
+                    </div>
+                </div>
+                <!-- Fin de agregar ciclo ---------->
+                <!--- -------------------- TABLA DE CICLOS ---------------------->
+                <div class="card">
+                    <div class="card-header">
+                    <h3 class="card-title">CICLOS FUTUROS</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body p-0">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th style="width: 10px">#</th>
+                          <th>Ciclos</th>
+                          <th>Acción</th>
+                          <th>Estado</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1.</td>
+                          <td>junio - agosto</td>
+                          <td>
+                            <button type="button" class="btn btn-block btn-outline-primary btn-xs">Configurar</button>
+                          </td>
+                          <td><span class="badge bg-danger">Actual</span></td>
+                        </tr>
+                        <tr>
+                          <td>2.</td>
+                          <td>septiembre - diciembre</td>
+                          <td>
+                            <button type="button" class="btn btn-block btn-outline-primary btn-xs">Configurar</button>
+                          </td>
+                          <td><span class="badge bg-info">Futuro</span></td>
+                        </tr>
+                        
+                      </tbody>
+                    </table>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!----------------------FIN DE LA TABLA DE CICLOS--------------->
+          </div>
+        </div>
         <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
     </section>
@@ -105,52 +229,102 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- OPTIONAL SCRIPTS -->
-<script src="../dist/js/demo.js"></script>
-
 <!-- jQuery -->
 <script src="../plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
 <!-- Bootstrap 4 -->
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="../plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="../plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="../plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="../plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="../plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
+<!-- Select2 -->
+<script src="../plugins/select2/js/select2.full.min.js"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- InputMask -->
 <script src="../plugins/moment/moment.min.js"></script>
+<script src="../plugins/inputmask/jquery.inputmask.min.js"></script>
+<!-- date-range-picker -->
 <script src="../plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
 <script src="../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="../plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- Bootstrap Switch -->
+<script src="../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../dist/js/adminlte.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="../dist/js/pages/dashboard.js"></script>
+<script src="../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../dist/js/demo.js"></script>
-
-<!-- jQuery Mapael -->
-<script src="../plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="../plugins/raphael/raphael.min.js"></script>
-<script src="../plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="../plugins/jquery-mapael/maps/usa_states.min.js"></script>
-<!-- ChartJS -->
-<script src="../plugins/chart.js/Chart.min.js"></script>
-<!-- PAGE SCRIPTS -->
-<script src="../dist/js/pages/dashboard2.js"></script>
+<!-- Page script -->
 </body>
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Initialize Select2 Elements
+    $('.select2bs4').select2({
+      theme: 'bootstrap4'
+    })
+
+    //Datemask dd/mm/yyyy
+    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+    //Datemask2 mm/dd/yyyy
+    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+    //Money Euro
+    $('[data-mask]').inputmask()
+
+    //Date range picker
+    $('#reservationdate').datetimepicker({
+        format: 'L'
+    });
+    //Date range picker
+    $('#reservation').daterangepicker()
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({
+      timePicker: true,
+      timePickerIncrement: 30,
+      locale: {
+        format: 'MM/DD/YYYY hh:mm A'
+      }
+    })
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+      {
+        ranges   : {
+          'Today'       : [moment(), moment()],
+          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment().subtract(29, 'days'),
+        endDate  : moment()
+      },
+      function (start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+      }
+    )
+
+    //Timepicker
+    $('#timepicker').datetimepicker({
+      format: 'LT'
+    })
+
+    //Bootstrap Duallistbox
+    $('.duallistbox').bootstrapDualListbox()
+
+    //Colorpicker
+    $('.my-colorpicker1').colorpicker()
+    //color picker with addon
+    $('.my-colorpicker2').colorpicker()
+
+    $('.my-colorpicker2').on('colorpickerChange', function(event) {
+      $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
+    });
+
+    $("input[data-bootstrap-switch]").each(function(){
+      $(this).bootstrapSwitch('state', $(this).prop('checked'));
+    });
+
+  })
+</script>
 </html>
