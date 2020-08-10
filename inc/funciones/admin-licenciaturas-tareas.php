74f3c4d8053c65c365dcff1e3b7b92a200a69700
satswere <verjruhfukeufn@gmail.com>
2020-08-10 01:30:55 -0500

    materias, ajustes menores a licenciaturas
    se agrego la seccion funcional de materias, falta la implentacion de ajax.
    funcionamiento de esta version:
    licenciaturas.php:
    -agregar licenciatura(ajax)
    -agregar licenciatura activa
    
    materias:
    -agregar materias
    -filtrado de licenciaturas activas
    
    correcto funcionamiento de ambas secciones, se agrego las correspondientes funciones que utliza las materias.
    
    consultar slack en caso de problemas: posibles errores, cambiar:
    inc\funciones\conexion.php
    por los datos que se tenga de su base de datos.
    actualizacion de las tablas en la base de datos(consultar actualizaciones de slack)

--- inc/funciones/admin-licenciaturas-tareas.php
+++ inc/funciones/admin-licenciaturas-tareas.php
@@ -2,19 +2,18 @@
 $operacion = $_POST['operacion'];
 
 switch ($operacion) {
-  case 'AgregarLicenciatura': //agregar licenciatura en el case
-    $Nombre_tarea = $_POST['Nombre_tarea'];
-    $id = $_POST['id_licenciatura'];
-    
+  //*********************agregar licenciatura en el case************************
+  case 'AgregarLicenciatura':
+    $NombreLicenciatura = $_POST['NombreLicenciatura'];
     include 'conexion.php';
     try{
-      //consulta a la base de datos
+
       $stmt = $conn->prepare("INSERT INTO licenciatura (NombreLic) VALUES(?)");
       $stmt->bind_param('s',$NombreLicenciatura);
       $stmt->execute(); 
     
-      if($stmt->affected_rows > 0){  //si la columna de la tabla ha sido modicada entonces
-        $respuesta = array(   //enviar esta respuesta (se puede cambiar)
+      if($stmt->affected_rows > 0){
+        $respuesta = array( 
           'respuesta' => 'correcto',
           'id_licenciatura' => $stmt->insert_id,
           'Nombre_lic' => $NombreLicenciatura
@@ -25,31 +24,30 @@ switch ($operacion) {
         );
       }
     
-      $stmt->close(); //termina la sentencia
-      $conn->close(); //termina la conexion
+      $stmt->close();
+      $conn->close(); 
     
     }catch(Exception $e){
-      //tomar la Exception
       $respuesta = array(
         'error' => $e->getMessage()
       );
     }
     echo json_encode($respuesta);
     break;
-      case 'agregar_materia': //agregar materia seccion del case
- 
+//    ************agregar Licenciatura_activa seccion del case************************
+      case 'agregar_materia':
         $id_recibido = $_POST['id_licenciatura'];
         $ciclo = $_POST['ciclo'];
     
     include 'conexion.php';
     try{
-      //consulta a la base de datos
+    
       $stmt = $conn->prepare("INSERT INTO licenciatura_activa (ID_Lincenciatura,ID_Ciclo) VALUES(?,?)");
       $stmt->bind_param('ii',$id_recibido,$ciclo);
       $stmt->execute(); 
     
-      if($stmt->affected_rows > 0){  //si la columna de la tabla ha sido modicada entonces
-        $respuesta = array(   //enviar esta respuesta (se puede cambiar)
+      if($stmt->affected_rows > 0){  
+        $respuesta = array(  
           'como_respuesta' => 'correcto',
           'id_licenciatura' => $stmt->insert_id,
           'Nombre_lic' => $id_recibido
@@ -60,20 +58,47 @@ switch ($operacion) {
         );
       }
     
-      $stmt->close(); //termina la sentencia
-      $conn->close(); //termina la conexion
+      $stmt->close(); 
+      $conn->close(); 
     
     }catch(Exception $e){
-      //tomar la Exception
       $respuesta = array(
         'error' => $e->getMessage()
       );
     }
     echo json_encode($respuesta);
-     
         break;
-        case 'existe_correo':
+        //********** */  FUNCION AGREGAR NUEVA METERIA ******************
+        case 'agregar_nueva_materia':
+          $id_Lic_Activa = $_POST['id_Lic_Recibida'];
+          $Nombre_asignatura = $_POST['Nueva_tarea'];
+       include 'conexion.php';
+       try{
+
+         $stmt = $conn->prepare("INSERT INTO asignatura (Nombre_Asignatura,ID_LicActiva) VALUES(?,?)");
+         $stmt->bind_param('si',$Nombre_asignatura,$id_Lic_Activa);
+         $stmt->execute(); 
        
+         if($stmt->affected_rows > 0){  
+           $respuesta = array(   
+             'como_respuesta' => 'correcto',
+             'id_asignatura' => $stmt->insert_id,
+             'Nombre_Asignatura' => $Nombre_asignatura
+           );
+         }else{
+           $respuesta = array(
+             'respuesta' => 'error'
+           );
+         }   
+         $stmt->close(); 
+         $conn->close(); 
+
+       }catch(Exception $e){
+         $respuesta = array(
+           'error' => $e->getMessage()
+         );
+       }
+       echo json_encode($respuesta);
           break;
   default:
     console.log('inexistente');
