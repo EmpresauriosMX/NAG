ad25ea7bbf76202e367cf233fa86343365918a51
feltydany <feltydany.dgm@gmail.com>
2020-08-14 19:12:21 -0500

    Inserta datos en Instituto prueba

--- admin/agde.php
+++ admin/agde.php
@@ -15,9 +15,9 @@
     //Verifico si hay datos en la BD o no
     $res=mysqli_query($link,"SELECT * from instituto");
     $result=mysqli_query($link,"SELECT COUNT(id_instituto) from instituto");
-    echo"ANTES DEL WHILE";
+ /*   echo"ANTES DEL WHILE";
     while($row=mysqli_fetch_array($res)){
-/*
+
        echo"resultado de la consulta es : $row[id_instituto]";
        echo'La variable row es:  '.$row[id_instituto].'';
        echo"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
@@ -28,34 +28,26 @@
            echo"Entro al IF";
        }else{
             echo"Entro al ELSE :/";
-       }*/
-}
+       }
+}*/
 
-if(0 == $row["id_instituto"]){
-    echo"Entro al IF";
+if(0 == $res){
+    //echo"Entro al IF";
     //Insersion de datos a la tabla "instituto" en la BD
         mysqli_query($link,"INSERT INTO instituto (id_instituto,nombre,clave,tipo_de_institucion,descripcion,direccion,telefono,correo) 
         VALUES(1,'$nominst','$clave','$tipo','$desc','$direc','$tel','$correo')");
-        echo "<meta http-equiv='REFRESH'content='5;URL=institucion.php'>";
+        //echo "<meta http-equiv='REFRESH'content='0;URL=institucion.php'>";
         echo"Se insertaron datos por primera vez en la tabla";
-}elseif(1 == $row["id_instituto"]){
-     echo"Entro al ELSE :/";
-     echo"Ya existe una institución";
-        echo"Se actualizaran Datos";
+}elseif(1 == $res){
+     //echo"Entro al ELSE :/";
+     //echo"Ya existe una institución";
+        //echo"Se actualizaran Datos";
         //Actualiza datos de la tabla "instituto" en la BD
         mysqli_query($link,"UPDATE instituto SET nombre='$nominst',clave='$clave',tipo_de_institucion='$tipo',descripcion='$desc'
         ,direccion='$direc',telefono='$tel',correo='$correo' WHERE id_instituto=1");
-        echo "<meta http-equiv='REFRESH'content='5;URL=institucion.php'>";
+        echo "<meta http-equiv='REFRESH'content='0;URL=institucion.php'>";
 }else{
-    echo"Pinches problemas";
+    echo"OCURRIO UN PROBLEMA EN EL IF";
 }
 
- //Si no hay datos en la BD inserto nuevos datos, si si hay solo actualizo datos
-    /*
-    if(0 == $row["id_instituto"]){
-        
-    }else{
-        
-    }
-*/
 ?>
